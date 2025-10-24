7e7c7812a1ad41eb7ead2c401ff65e8c5f5a929e
cesar7722a <cesarfilipeantonio2404@gmail.com>
2025-10-24 20:06:54 +0100

    feat:install lucide-react icon

--- src/private/private-layout.tsx
+++ src/private/private-layout.tsx
@@ -3,13 +3,14 @@ import { Outlet } from "react-router";
 function PrivateLayout() {
   return (
     <div className="flex h-screen">
-      <aside className="w-56 bg-red-400 flex flex-col">
-        <header className="h-10 bg-blue-300 flex items-center justify-center">
-          Header
+      <aside className="w-56 bg-blue-2 flex flex-col">
+        <header className="h-12 p-2 text-xl font-bold border border-t-0 border-l-0 border-r-0 border-blue-6">
+          Bank
         </header>
 
-        <div className="flex-1 p-4 overflow-y-auto h-72">
-          Sou fixa na lateral!
+        <div className="flex-1 p-2 overflow-y-auto h-72 space-y-2">
+          <span className="text-sm">Home</span>
+          <div></div>
         </div>
 
         <footer className="h-16 bg-slate-300 flex items-center justify-center">
