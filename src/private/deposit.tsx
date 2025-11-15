bb53d86425a127a00fbdf11994b4826081393926
cesar7722a <cesarfilipeantonio2404@gmail.com>
2025-11-15 20:17:45 +0100

    feat:create page deposit

--- src/private/deposit.tsx
+++ src/private/deposit.tsx
@@ -1,3 +1,106 @@
-export default function Deposit() {
-  return <div>hello deposit!!</div>;
+import { useState } from "react";
+import { Heading } from "../components/heading";
+import { Input } from "../components/input";
+import { Modal } from "../components/modal";
+import { useForm } from "react-hook-form";
+import { clients } from "../type-data/data-user";
+import type { Client } from "../type-data/type/clients-type";
+
+type FormData = {
+  number_account: string;
+  Amount_deposit: number;
+};
+
+export default function Transfer() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [dataUserTransfer, setDataUserTransfer] = useState<Client>();
+  const { control, handleSubmit } = useForm<FormData>();
+  const [amount, setAmount] = useState<number>();
+  const onSubmit = (data: FormData) => {
+    const transferUser = clients.find(
+      (user) => user.account_number == data.number_account
+    );
+
+    setDataUserTransfer(transferUser);
+    setAmount(data.Amount_deposit);
+  };
+  return (
+    <div className="space-y-10">
+      <div>
+        <Heading>Deposit</Heading>
+        <h2>Fill in the form to make deposit</h2>
+      </div>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="bg-white max-w-4xl mx-auto rounded-md px-6 pt-6 pb-8 space-y-4 shadow-md"
+      >
+        <div className="space-y-2">
+          <p className="text-xl">Enter the starting account details.</p>
+          <div className="flex gap-x-10">
+            <Input
+              label="Number of account"
+              control={control}
+              name="number_account"
+            />
+            <Input
+              label="Amount to be deposit"
+              type="number"
+              control={control}
+              name="Amount_deposit"
+            />
+          </div>
+        </div>
+        <div className="pt-10">
+          <button
+            onClick={() => setIsOpen(true)}
+            type="submit"
+            className="h-10 w-full border rounded-md bg-blue-9 hover:bg-blue-10 text-blue-contrast"
+          >
+            Confirmar
+          </button>
+        </div>
+        {isOpen && (
+          <Modal
+            className="flex justify-center items-center"
+            onClick={() => setIsOpen(false)}
+          >
+            <div
+              onClick={(e) => {
+                e.stopPropagation();
+              }}
+              className="w-[520px] bg-white border-blue-4 border rounded-md shadow-md p-3 py-4 space-y-6"
+            >
+              <h2 className="text-center text-2xl font-semibold">
+                confirm deposit
+              </h2>
+              <div>
+                <strong>Deposit in:</strong>
+                <p>
+                  name: <strong>{dataUserTransfer?.name}</strong>
+                </p>
+                <p>
+                  number account:
+                  <strong>{dataUserTransfer?.account_number}</strong>
+                </p>
+                <p>
+                  Amount: <strong>{amount} kz</strong>
+                </p>
+              </div>
+              <div className="flex justify-end gap-x-4">
+                <button
+                  onClick={() => setIsOpen(false)}
+                  className="border border-blue-9 px-6 rounded-md hover:bg-blue-2"
+                >
+                  Cancel
+                </button>
+                <button className="h-10 px-6 border rounded-md bg-blue-9 hover:bg-blue-10 text-blue-contrast">
+                  confirm
+                </button>
+              </div>
+            </div>
+          </Modal>
+        )}
+      </form>
+    </div>
+  );
 }
